c18d6bc06d31c7eed5272b1829151c89f0ee3ad7
Grettiro <grettir10@ru.is>
2015-01-20 12:37:27 -0800

    Test commit
    Didn't work before

---
+++ Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+		private bool gravity;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+			if (Input.GetButtonDown ("Jump"))
+								jump = true;
+			if (Input.GetKeyDown("d"))
+			{
+					gravity = true;
+			}	
+        }
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+			float h = Input.GetAxis ("Horizontal");
+            // Pass all parameters to the character control script.
+            character.Move(h, jump, gravity);
+			gravity = false;
+            jump = false;
+        }
+    }
+}
